4d8f7a5d50c6bfec9d71e5a741a919a004c4ca7b
laoyening <1337849843@qq.com>
2020-01-16 07:00:25 +0000

    增加sd卡启动后，自动扩容功能

--- boot/generic-startup.sh
+++ boot/generic-startup.sh
@@ -61,31 +61,71 @@ if [ -f /boot/efi/EFI/efi.gen ] ; then
 fi
 
 #Resize drive when requested
-if [ -f /resizerootfs ] ; then
-	echo "generic-board-startup: resizerootfs"
+if [ -d /home/debian/.resizerootfs ] ; then
+	ROOT_PART=$(mount | sed -n 's|^/dev/\(.*\) on / .*|\1|p')
 
-	unset is_btrfs
-	is_btrfs=$(cat /proc/cmdline | grep btrfs || true)
+  	PART_NUM=${ROOT_PART#mmcblk0p}
+  if [ "$PART_NUM" = "$ROOT_PART" ]; then
+	echo "$ROOT_PART is not an SD card. Don't expand"	
+  fi
 
-	if [ "x${is_btrfs}" = "x" ] ; then
-		drive=$(cat /resizerootfs)
-		if [ ! "x${drive}" = "x" ] ; then
-			echo "generic-board-startup: "
-			if [ "x${drive}" = "x/dev/mmcblk0" ] || [ "x${drive}" = "x/dev/mmcblk1" ] ; then
-				echo "generic-board-startup: resize2fs ${drive}p2"
-				resize2fs ${drive}p2 >/var/log/resize.log 2>&1 || true
-			else
-				echo "generic-board-startup: resize2fs ${drive}"
-				resize2fs ${drive} >/var/log/resize.log 2>&1 || true
-			fi
-		fi
-	else
-		echo "generic-board-startup: btrfs filesystem resize max /"
-		btrfs filesystem resize max / >/var/log/resize.log 2>&1 || true
-	fi
+  LAST_PART_NUM=$(parted /dev/mmcblk0 -ms unit s p | tail -n 1 | cut -f 1 -d:)
+  if [ $LAST_PART_NUM -ne $PART_NUM ]; then
+    echo "$ROOT_PART is not the last partition. Don't know how to expand"
+  fi
 
-	rm -rf /resizerootfs || true
-	sync
+  # Get the starting offset of the root partition
+  PART_START=$(parted /dev/mmcblk0 -ms unit s p | grep "^${PART_NUM}" | cut -f 2 -d: | sed 's/[^0-9]//g')
+  [ "$PART_START" ] || return 1
+  # Return value will likely be error for fdisk as it fails to reload the
+  # partition table because the root fs is mounted
+  fdisk /dev/mmcblk0 <<EOF
+p
+d
+$PART_NUM
+n
+p
+$PART_NUM
+$PART_START
+
+p
+w
+EOF
+  ASK_TO_REBOOT=1
+
+  # now set up an init.d script
+cat <<EOF > /etc/init.d/resize2fs_once &&
+#!/bin/sh
+### BEGIN INIT INFO
+# Provides:          resize2fs_once
+# Required-Start:
+# Required-Stop:
+# Default-Start: 3
+# Default-Stop:
+# Short-Description: Resize the root filesystem to fill partition
+# Description:
+### END INIT INFO
+
+. /lib/lsb/init-functions
+
+case "\$1" in
+  start)
+    log_daemon_msg "Starting resize2fs_once" &&
+    resize2fs /dev/$ROOT_PART &&
+    update-rc.d resize2fs_once remove &&
+    rm /etc/init.d/resize2fs_once &&
+    log_end_msg \$?
+    ;;
+  *)
+    echo "Usage: \$0 start" >&2
+    exit 3
+    ;;
+esac
+EOF
+  chmod +x /etc/init.d/resize2fs_once &&
+  update-rc.d resize2fs_once defaults &&
+  echo "Root partition has been resized.\nThe filesystem will be enlarged upon the next reboot"
+  rmdir /home/debian/.resizerootfs
 fi
 
 if [ -d /sys/class/gpio/ ] ; then
