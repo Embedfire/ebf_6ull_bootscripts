77e693bf0ae1837f67d1331634827674c3be5f61
laoyening <1337849843@qq.com>
2020-02-19 07:37:07 +0000

    wifi自启动脚本修正

--- boot/autowifi.sh
+++ boot/autowifi.sh
@@ -1,11 +1,11 @@
-[Unit]
-Description==booting wifi ...
-After=wpa_supplicant.service network.target local-fs.target udhcpd.service 
-Before=sshd.service
+#!/bin/bash
 
-[Service]
-Type=forking
-ExecStart=/bin/bash /opt/scripts/boot/autowifi.sh
+echo "root"|su -c ' ifconfig wlan0 up' root
 
-[Install]
-WantedBy=multi-user.target
+if [ -f /etc/wpa_supplicant/wifi.conf ] ; then
+
+  echo "root"|su -c 'wpa_supplicant -B -c /etc/wpa_supplicant/wifi.conf -iwlan0' root &
+
+fi
+
+echo "root"|su -c 'udhcpc -b -i wlan0' root
