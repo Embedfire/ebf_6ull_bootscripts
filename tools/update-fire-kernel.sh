91786ebf836bfdc63b48cd6db674ed874e86d697
laoyening <1337849843@qq.com>
2020-12-17 19:40:50 +0800

    update kernel
    Signed-off-by: laoyening <1337849843@qq.com>

--- tools/update-fire-kernel.sh
+++ tools/update-fire-kernel.sh
@@ -9,7 +9,9 @@ if [ ! -f /boot/vmlinuz-$(uname -r) ]; then
 	echo "error:fire kernel no exit!"
 else
 	cp -rf /boot /tmp	
-	
+
+	_do dpkg -r linux-image-$(uname -r)
+
 	_do apt update -y
 	
 	_do apt install linux-image-$(uname -r) -y
