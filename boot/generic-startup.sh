4a05c3840649463a158466cadc2b68741ae45223
laoyening <1337849843@qq.com>
2021-03-26 16:31:31 +0800

    remove time-consuming operations
    Signed-off-by: laoyening <1337849843@qq.com>

--- boot/generic-startup.sh
+++ boot/generic-startup.sh
@@ -18,48 +18,6 @@ if [ ! "x${are_we_flasher}" = "x" ] ; then
 	exit
 fi
 
-#Regenerate ssh host keys
-if [ -f /etc/ssh/ssh.regenerate ] ; then
-	echo "generic-board-startup: regenerating ssh keys"
-	systemctl stop sshd
-	rm -rf /etc/ssh/ssh_host_* || true
-
-	if [ -e /dev/hwrng ] ; then
-		# Mix in the output of the HWRNG to the kernel before generating ssh keys
-		dd if=/dev/hwrng of=/dev/urandom count=1 bs=4096 2>/dev/null
-		echo "generic-board-startup: if=/dev/hwrng of=/dev/urandom count=1 bs=4096"
-	else
-		echo "generic-board-startup: WARNING /dev/hwrng wasn't available"
-	fi
-
-	dpkg-reconfigure openssh-server
-
-	# while we're at it, make sure we have unique machine IDs as well
-	rm -f /var/lib/dbus/machine-id || true
-	rm -f /etc/machine-id || true
-	dbus-uuidgen --ensure
-	systemd-machine-id-setup
-
-	sync
-	if [ -s /etc/ssh/ssh_host_ed25519_key.pub ] ; then
-		rm -f /etc/ssh/ssh.regenerate || true
-		sync
-		systemctl start sshd
-	fi
-fi
-
-if [ -f /boot/efi/EFI/efi.gen ] ; then
-	if [ -f /usr/sbin/grub-install ] ; then
-		echo "grub-install --efi-directory=/boot/efi/ --target=arm-efi --no-nvram"
-		grub-install --efi-directory=/boot/efi/ --target=arm-efi --no-nvram
-		echo "update-grub"
-		update-grub
-		sync
-	fi
-	rm -rf /boot/efi/EFI/efi.gen || true
-	sync
-fi
-
 #Resize drive when requested
 if [ -d /home/.resizerootfs ] ; then
 	ROOT_PART=$(mount | sed -n 's|^/dev/\(.*\) on / .*|\1|p')
